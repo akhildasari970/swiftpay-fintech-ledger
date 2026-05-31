1336ba13bf1fca71c8a0c88a464d7294759c79d0
Dasari Akhil <dasariakhil970@gmail.com>
2026-06-01 00:50:20 +0530

    made changes to pom file

---
+++ ledger-service/ledger-service/src/main/java/com/swiftpay/ledger/LedgerServiceApplication.java
@@ -0,0 +1,7 @@
+package com.swiftpay.ledger;
+
+public class LedgerServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Ledger Service Started");
+    }
+}
