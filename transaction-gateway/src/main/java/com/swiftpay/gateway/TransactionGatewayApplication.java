25ff84aaac0869713f49ad0338a21144a50e4431
Dasari Akhil <dasariakhil970@gmail.com>
2026-05-31 22:56:43 +0530

    Phase 2.1 - Gateway bootstrap

---
+++ transaction-gateway/src/main/java/com/swiftpay/gateway/TransactionGatewayApplication.java
@@ -0,0 +1,7 @@
+package com.swiftpay.gateway;
+
+public class TransactionGatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("Transaction Gateway Bootstrap");
+    }
+}
