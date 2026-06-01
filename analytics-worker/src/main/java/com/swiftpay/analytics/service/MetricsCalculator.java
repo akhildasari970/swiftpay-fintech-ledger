613886ea898e73fc0e4611503b56f87297212008
Dasari Akhil <dasariakhil970@gmail.com>
2026-06-01 07:47:43 +0530

    Phase 4.4 - Analytics processing foundation

---
+++ analytics-worker/src/main/java/com/swiftpay/analytics/service/MetricsCalculator.java
@@ -0,0 +1,12 @@
+package com.swiftpay.analytics.service;
+
+public class MetricsCalculator {
+
+    public double calculateVolume(double amount) {
+        return amount;
+    }
+
+    public long incrementCount(long count) {
+        return count + 1;
+    }
+}
