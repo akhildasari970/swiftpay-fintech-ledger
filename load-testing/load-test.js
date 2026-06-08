04737c3974407fdbbcc1381a7a4b7b7c0cde7b5b
Dasari Akhil <dasariakhil970@gmail.com>
2026-06-08 09:23:51 +0530

    final version

--- load-testing/load-test.js
+++ load-testing/load-test.js
@@ -1,15 +1,29 @@
 import http from 'k6/http';
 
 export let options = {
-  vus: 50,
-  duration: '1m'
+    vus: 250,
+    duration: '10m'
 };
 
 export default function () {
-  http.get('http://localhost:8083/actuator/health');
-}
 
-//import http from 'k6/http';
-//export default function () {
-//  http.get('http://localhost:8081/actuator/health');
-//}
+    const payload = JSON.stringify({
+        transactionId: `TXN-${Date.now()}-${__VU}-${__ITER}`,
+        senderId: "USER1",
+        receiverId: "USER2",
+        amount: 100.50,
+        currency: "USD"
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    http.post(
+        'http://localhost:8081/v1/payments',
+        payload,
+        params
+    );
+}
